67a28b51bde7924a3c39aff1d7294628aa47964d
Nicolas Agathos <nicolas.agathos@gmail.com>
2022-02-17 18:24:03 +0100

    plant_tag + stimulus + select2

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,8 @@
 class Plant < ApplicationRecord
   belongs_to :garden
 
+  has_many :plant_tags # @plant.plant_tags => "Array" of  PlantTag instances
+  has_many :tags, through: :plant_tags  # @plant.tags => "Array" of Tag instances
+
   validates :name, :image_url, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   resources :gardens, only: %i[index show] do
     resources :plants, only: %i[new create]
   end
-  resources :plants, only: [:destroy]
+  resources :plants, only: [:destroy] do
+    resources :plant_tags, only: [:new, :create]
+  end
 end
+
+# PlantTag                FORM                                 Submit Form
+# - plant (URL=> GET /plants/:plant_id/plant_tag/new), POST /plants/:plant_id/plant_tags
+
+# - tag (FORM)
